0596b864b3ce73df57caceb7b21452b529644d85
Mariam-Ashraf-BC <mariem.ashraf.kamel@gmail.com>
2026-06-08 11:42:11 +0300

    data-compression

--- data-compression.js
+++ data-compression.js
@@ -6,7 +6,23 @@
  
  const faker = require('faker'); //to generate items with random data
  const moment = require('moment'); //to generate timestamps
+ const zlib = require('zlib'); //to compress
  
+ generateNotesItem((item) => {
+    console.log("uncompressed item -> ", item)
+    putNotesItem(item, (err,data) =>{
+        if(err) console.log(err);
+        //else console.log("dataa-> ", data)
+        console.log("Compressed item -> ", item)
+        getNotesItem({
+            user_id: item.user_id,
+            timestamp: item.timestamp
+        }, (err,data) => {
+           if(err) console.log(err);
+           else console.log("uncompressed read", data.Item);
+        })
+    })
+ })
 
 
  function generateNotesItem(callback) {
@@ -18,14 +34,56 @@
         title: faker.random.words(),
         content: faker.hacker.phrase(),
         user_name: faker.internet.userName(),
-        expires: moment().unix() + 3600, //expires in 1 hour
+        expires: moment().unix() + 3600, //expires in 1 hour,
     })
 }
 
 function putNotesItem(item, callback) {
+    if(item.content.length > 35) {
+        zlib.gzip(item.content, (e,content_b)=>{
+            item.content_b = content_b
+            item.content = undefined
+            docClient.put({
+                TableName: "global_td_notes",
+                Item: item
+            }, callback)
+        })
+
+    }
+    else{
+        docClient.put({
+                TableName: "global_td_notes",
+                Item: item
+            }, callback)
+    }
 
 }
 
 function getNotesItem(key, callback){
-    
+    docClient.get({
+        TableName: "global_td_notes",
+        Key: key
+    }, (err,data) => {
+        if(err) {
+            callback(err)
+        }
+        else {
+            if(data.Item.content){
+                callback(null,data)
+            }
+            else{
+                zlib.gunzip(data.Item.content_b, (err, content) =>{
+                    if(err) {
+                        callback(err)
+                    }
+                    else{
+                        data.Item.content = content.toString();
+                        data.Item.content_b = undefined;
+                        callback(null, data);
+                    }
+                })
+            }
+        }
+    })
+
 }
