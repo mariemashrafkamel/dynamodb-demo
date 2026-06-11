4dae5bf32dfcf242ab5a31dbbfb48113102f6f43
Mariam-Ashraf-BC <mariem.ashraf.kamel@gmail.com>
2026-06-11 14:34:18 +0300

    update region

--- read-ops-cloudtrail.js
+++ read-ops-cloudtrail.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
-AWS.config.update({ region: 'us-west-2' });
-
+//AWS.config.update({ region: 'us-west-2' });
+AWS.config.update({ region: 'us-east-1' });
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 docClient.get({
